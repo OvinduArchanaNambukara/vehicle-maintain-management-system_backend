68cc2207c3440534c54d3d4533f5eaa12bdbfdda
ovindu <ovindunambukara@gmail.com>
2021-03-09 18:58:39 +0530

    initial commit

---
+++ src/main/java/com/springboot/vmms/VmmsApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.vmms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VmmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VmmsApplication.class, args);
+    }
+
+}
