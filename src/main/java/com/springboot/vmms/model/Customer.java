7c3a61fdf6dc805a96e005e3fb993165fad4ff4f
ovindu <ovindunambukara@gmail.com>
2021-03-10 15:21:24 +0530

    rename table columns

--- src/main/java/com/springboot/vmms/model/Customer.java
+++ src/main/java/com/springboot/vmms/model/Customer.java
@@ -7,31 +7,31 @@ import javax.persistence.Id;
 public class Customer {
     @Id
     private String id;
-    private String first_name;
-    private String last_name;
+    private String firstName;
+    private String lastName;
     private String address;
     private String city;
     private String province;
     private String zip;
     private String email;
     private String telephone;
-    private String national_id;
+    private String nationalId;
 
     public Customer() {
     }
 
-    public Customer(String id, String first_name, String last_name, String address, String city, String province, String zip,
-                    String email, String telephone, String national_id) {
+    public Customer(String id, String firstName, String lastName, String address, String city, String province, String zip,
+                    String email, String telephone, String nationalId) {
         this.id = id;
-        this.first_name = first_name;
-        this.last_name = last_name;
+        this.firstName = firstName;
+        this.lastName = lastName;
         this.address = address;
         this.city = city;
         this.province = province;
         this.zip = zip;
         this.email = email;
         this.telephone = telephone;
-        this.national_id = national_id;
+        this.nationalId = nationalId;
     }
 
     public String getId() {
@@ -42,20 +42,20 @@ public class Customer {
         this.id = id;
     }
 
-    public String getFirst_name() {
-        return first_name;
+    public String getFirstName() {
+        return firstName;
     }
 
-    public void setFirst_name(String first_name) {
-        this.first_name = first_name;
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
     }
 
-    public String getLast_name() {
-        return last_name;
+    public String getLastName() {
+        return lastName;
     }
 
-    public void setLast_name(String last_name) {
-        this.last_name = last_name;
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
     }
 
     public String getAddress() {
@@ -106,11 +106,11 @@ public class Customer {
         this.telephone = telephone;
     }
 
-    public String getNational_id() {
-        return national_id;
+    public String getNationalId() {
+        return nationalId;
     }
 
-    public void setNational_id(String national_id) {
-        this.national_id = national_id;
+    public void setNationalId(String nationalId) {
+        this.nationalId = nationalId;
     }
 }
